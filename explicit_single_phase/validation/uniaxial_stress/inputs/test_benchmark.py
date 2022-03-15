1d9ab354a5c897a6513d298802bea4a1aeefc69c
jgiven100 <joelgiven@berkeley.edu>
2022-03-15 13:04:20 -0700

    off by 1, update output file

--- explicit_single_phase/validation/uniaxial_stress/inputs/test_benchmark.py
+++ explicit_single_phase/validation/uniaxial_stress/inputs/test_benchmark.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Get current path
 os.chdir(pathlib.Path(__file__).parent.absolute())
 # Read results as DF
-df = pd.read_hdf('results/uniaxial-stress-2d-usf/particles09.h5', 'table')
+df = pd.read_hdf('results/uniaxial-stress-2d-usf/particles10.h5', 'table')
 # Maximum stress in yy and xx
 syy = -0.9999999999999999
 sxx = 0.0
--- explicit_single_phase/validation/uniaxial_traction/inputs/test_benchmark.py
+++ explicit_single_phase/validation/uniaxial_traction/inputs/test_benchmark.py
@@ -7,42 +7,42 @@ os.chdir(pathlib.Path(__file__).parent.absolute())
 # Nodal forces results
 ## Step 300
 df = pd.read_hdf('results/uniaxial-nodal-forces-2d/particles0300.h5', 'table')
-assert round(df['stress_xx'].min() - 0.5925210678182377, 8) == 0.0
-assert round(df['stress_xx'].max() - 0.5974539476363379, 8) == 0.0
+assert round(df['stress_xx'].min() - 0.590520812744999, 8) == 0.0
+assert round(df['stress_xx'].max() - 0.595453247132066, 8) == 0.0
 
 ## Step 510
 df = pd.read_hdf('results/uniaxial-nodal-forces-2d/particles0510.h5', 'table')
-assert round(df['stress_xx'].min() - 1.0026665338366039, 8) == 0.0
-assert round(df['stress_xx'].max() - 1.0112231542459431, 8) == 0.0
+assert round(df['stress_xx'].min() - 1.0024898572800125, 8) == 0.0
+assert round(df['stress_xx'].max() - 1.0107003439391022, 8) == 0.0
 
 ## Step 750
 df = pd.read_hdf('results/uniaxial-nodal-forces-2d/particles0750.h5', 'table')
-assert round(df['stress_xx'].min() - 1.0000053000532143, 8) == 0.0
-assert round(df['stress_xx'].max() - 1.0000225119807862, 8) == 0.0
+assert round(df['stress_xx'].min() - 1.0000057400725588, 8) == 0.0
+assert round(df['stress_xx'].max() - 1.0000241541441601, 8) == 0.0
 
 ## Step 990
-df = pd.read_hdf('results/uniaxial-nodal-forces-2d/particles0990.h5', 'table')
-assert round(df['stress_xx'].min() - 0.9999990078443788, 8) == 0.0
-assert round(df['stress_xx'].max() - 0.9999990292713694, 8) == 0.0
+df = pd.read_hdf('results/uniaxial-nodal-forces-2d/particles1000.h5', 'table')
+assert round(df['stress_xx'].min() - 0.999998970020447, 8) == 0.0
+assert round(df['stress_xx'].max() - 0.999998991968051, 8) == 0.0
 
 
 # Particle traction results
 ## Step 300
 df = pd.read_hdf('results/uniaxial-particle-traction-2d/particles0300.h5', 'table')
-assert round(df['stress_xx'].min() - 0.4450086768966724, 8) == 0.0
-assert round(df['stress_xx'].max() - 0.5966527842046769, 8) == 0.0
+assert round(df['stress_xx'].min() - 0.4435084213078205, 8) == 0.0
+assert round(df['stress_xx'].max() - 0.5946521054012821, 8) == 0.0
 
 ## Step 510
 df = pd.read_hdf('results/uniaxial-particle-traction-2d/particles0510.h5', 'table')
-assert round(df['stress_xx'].min() - 0.7528092313640623, 8) == 0.0
-assert round(df['stress_xx'].max() - 1.0109599915279937, 8) == 0.0
+assert round(df['stress_xx'].min() - 0.7526811177289789, 8) == 0.0
+assert round(df['stress_xx'].max() - 1.0104848452100940, 8) == 0.0
 
 ## Step 750
 df = pd.read_hdf('results/uniaxial-particle-traction-2d/particles0750.h5', 'table')
-assert round(df['stress_xx'].min() - 0.7500090055681591, 8) == 0.0
-assert round(df['stress_xx'].max() - 1.0000224746314728, 8) == 0.0
+assert round(df['stress_xx'].min() - 0.7500094308335878, 8) == 0.0
+assert round(df['stress_xx'].max() - 1.0000240617444776, 8) == 0.0
 
 ## Step 990
 df = pd.read_hdf('results/uniaxial-particle-traction-2d/particles0990.h5', 'table')
-assert round(df['stress_xx'].min() - 0.750002924022295, 8) == 0.0
-assert round(df['stress_xx'].max() - 0.9999997782938734, 8) == 0.0
+assert round(df['stress_xx'].min() - 0.7500029258993646, 8) == 0.0
+assert round(df['stress_xx'].max() - 0.9999997852991345, 8) == 0.0
