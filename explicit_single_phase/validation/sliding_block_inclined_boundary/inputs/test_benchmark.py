aba360d9c440a6f830798db29d047ce09dc914cf
Bodhinanda Chandra <bodhinandach@gmail.com>
2022-03-15 10:38:02 -0700

    Update test_benchmark.py

--- explicit_single_phase/validation/sliding_block_inclined_boundary/inputs/test_benchmark.py
+++ explicit_single_phase/validation/sliding_block_inclined_boundary/inputs/test_benchmark.py
@@ -10,17 +10,12 @@ df1 = pd.read_hdf('results/boundary_inclined_friction/particles-1_4-099000.h5', 
 df2 = pd.read_hdf('results/boundary_inclined_friction/particles-2_4-099000.h5', 'table')
 df3 = pd.read_hdf('results/boundary_inclined_friction/particles-3_4-099000.h5', 'table')
 df = df0
-df.append(df1)
-df.append(df2)
-df.append(df3)
+df = df.append(df1)
+df = df.append(df2)
+df = df.append(df3)
 
-print(df['coord_x'].min())
-print(df['coord_x'].max())
-print(df['coord_y'].min())
-print(df['coord_y'].max())
-
-## Assert location of particles
-assert round(df['coord_x'].min() - 3.285773141443035, 8) == 0.0
-assert round(df['coord_x'].max() - 3.9749404708911587, 8) == 0.0
-assert round(df['coord_y'].min() - (-2.2047220925269966), 8) == 0.0
-assert round(df['coord_y'].max() - (-1.6070738620774836), 8) == 0.0
+# Assert location of particles
+assert round(df['coord_x'].min() - 3.1517918575298047, 8) == 0.0
+assert round(df['coord_x'].max() - 4.432431861326462, 8) == 0.0
+assert round(df['coord_y'].min() - -2.2523488456520457, 8) == 0.0
+assert round(df['coord_y'].max() - -0.9717052975981368, 8) == 0.0
